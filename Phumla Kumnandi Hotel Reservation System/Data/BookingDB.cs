21696a0ff350beb78b4b96f6cb4faee4465ba71d
Deshy Dan <deshy733@gmail.com>
2023-10-06 00:13:05 +0200

    Add project files.

---
+++ Phumla Kumnandi Hotel Reservation System/Data/BookingDB.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kumnandi_Hotel_Reservation_System.Data
+{
+    public class BookingDB:DB
+    {
+
+        #region data members
+        private string bookingTable = "bookings";
+        private string sqlLocal1 = "SELECT * FROM bookings";
+        private Collection<Booking> bookings;
+
+
+        #region constructor 
+        public BookingDB(): base()
+        {
+            bookings = new Collection<Booking> ();
+            FillDataSet(sqlLocal1, bookingTable);
+            AddToCollection(bookingTable); 
+
+        }
+
+        #endregion
+
+
+        #region Utility methods
+        public DataSet GetDataSet()
+        {
+            return dataSet;
+        }
+
+        private void AddToCollection(string table)
+        {
+            DataRow myRow = null;
+            Booking booking; 
+            foreach(DataRow row in dataSet.Tables[table].Rows)
+            {
+                myRow = row; 
+                if(!(myRow.RowState == DataRowState.Deleted))
+                {
+                    booking = new Booking();
+                    booking.Id = Convert.ToInt32(myRow["id"]);
+                    booking.RoomId = Convert.ToInt32(myRow["roomId"]);
+                    booking.BookingStatusId = Convert.ToString(myRow["bookingStatusId"]).TrimEnd();
+                    booking.ArrivalDate = Convert.ToString(myRow["arrivalDate"]).TrimEnd();
+                    booking.TotalAmount = Convert.ToString(myRow["totalAmount"]).TrimEnd();
+                    booking.Deposit = Convert.ToString(myRow["deposit"]).TrimEnd();
+                    booking.NumberOfGuest = Convert.ToString(myRow["numberOfGuest"]).TrimEnd();
+                    booking.SpecialRequest = Convert.ToString(myRow["specialRequest"]).TrimEnd();
+                }
+                bookings.Add(booking);
+            }
+           
+        }
+
+        private void FillRow(DataRow row, Booking booking, DB.DBOperation operation)
+        {
+            if (operation == DB.DBOperation.Add)
+            {
+
+                row["id"] = booking.Id;
+                row["idNumber"] = booking.IdNumber;
+
+            }
+            row["title"] = booking.Title;
+            row["firstName"] = booking.FirstName;
+            row["LastName"] = booking.LastName;
+            row["email"] = booking.Email;
+            row["telephone"] = booking.Telephone;
+            row["address"] = booking.Address;
+        }
+        private int FindRow(Booking booking, string table)
+        {
+            int rowIndex = 0;
+            DataRow myRow;
+            int returnValue = -1;
+            foreach (DataRow row in dataSet.Tables[table].Rows)
+            {
+                myRow = row;
+                if (myRow.RowState != DataRowState.Deleted)
+                {
+                    if (booking.Id == Convert.ToString(dataSet.Tables[table].Rows[rowIndex]["id"]))
+                    {
+                        returnValue = rowIndex;
+                    }
+                }
+                rowIndex++;
+            }
+
+            return returnValue;
+        }
+
+        #endregion
+
+        #region CRUD operataions
+        public void DataSetChange(Booking booking, DB.DBOperation operation)
+        {
+
+            DataRow row = null;
+            string dataTable =bookingTable;
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    row = dataSet.Tables[dataTable].NewRow();
+                    FillRow(row, booking, operation);
+                    dataSet.Tables[dataTable].Rows.Add(row);
+                    break;
+                case DB.DBOperation.Edit:
+                    row = dataSet.Tables[dataTable].Rows[FindRow(booking, dataTable)];
+                    FillRow(row, booking, operation);
+                    break;
+                case DB.DBOperation.Delete:
+                    int rowIndexToDelete = FindRow(booking, dataTable);
+                    if (rowIndexToDelete != -1)
+                    {
+                        dataSet.Tables[dataTable].Rows.RemoveAt(rowIndexToDelete);
+                    }
+                    break;
+
+            }
+        }
+
+        #endregion
+        #region build parameters, create commands and update database
+        private void insert(Booking booking)
+        {
+            SqlParameter param = default(SqlParameter);
+            param = new SqlParameter("@id", SqlDbType.Int, 50, "id");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@roomId", SqlDbType.NChar, 13, "roomId");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@bookingStatusId", SqlDbType.NChar, 13, "bookingStatusId");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@arrivalDate", SqlDbType.NVarChar, 50, "arrivalDate");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@totalAmount", SqlDbType.NVarChar, 50, "totalAmount");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+
+            param = new SqlParameter("@deposit", SqlDbType.NVarChar, 50, "deposit");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@numberOfGuests", SqlDbType.NVarChar, 50, "numberOfGuests");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+
+            param = new SqlParameter("@specialRequest", SqlDbType.NVarChar, 50, "specialRequest");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+        }
+
+
+            private void update(Guest guest)
+        {
+            SqlParameter param = default(SqlParameter);
+
+            param = new SqlParameter("@originalId", SqlDbType.Int, 50, "id");
+            param.SourceVersion = DataRowVersion.Original;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+
+            param = new SqlParameter("@roomId", SqlDbType.NChar, 13, "roomId");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@bookingStatusId", SqlDbType.NChar, 13, "bookingStatusId");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@arrivalDate", SqlDbType.NVarChar, 50, "arrivalDate");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@totalAmount", SqlDbType.NVarChar, 50, "totalAmount");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param); ;
+
+
+            param = new SqlParameter("@deposit", SqlDbType.NVarChar, 50, "deposit");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@numberOfGuests", SqlDbType.NVarChar, 50, "numberOfGuests");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+
+
+            param = new SqlParameter("@specialRequest", SqlDbType.NVarChar, 50, "specialRequest");
+            param.SourceVersion = DataRowVersion.Current;
+            dataAdapter.UpdateCommand.Parameters.Add(param);
+        }
+
+        private void Insert(Booking booking)
+        {
+            dataAdapter.InsertCommand = new SqlCommand(
+                "INSERT INTO booking(roomId, guestId, bookingStatusId, arrivalDate, totalAmount, deposit, numberOfGuest, specialRequest) values(@roomId, @guestId, @bookingStatusId, @arrivalDate, @totalAmount, @deposit, @numberOfGuest, @specialRequest)"
+                );
+
+            insert(booking);
+        }
+
+        public bool UpdateDataSource(Booking booking)
+        {
+            bool sucess = true;
+            insert(booking);
+            update(booking);
+
+
+            UpdateDataSource(sqlLocal1, bookingTable);
+
+            return sucess;
+        }
+        #endregion
+        #region getters and setters
+        public Collection<Booking> AllBookings
+        {
+            get
+            {
+                return bookings;
+            }
+        }
+     
+
+
+        #endregion
+
+    }
+}
---
+++ Phumla Kumnandi Hotel Reservation System/Data/RoomDB.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kumnandi_Hotel_Reservation_System.Data
+{
+    public  class RoomDB :DB
+    {
+
+        #region instance variables 
+        private string guestTable = "room";
+        private string sqlLocal1 = "SELECT * FROM room";
+        private Collection<Room> rooms;
+
+        #endregion
+
+
+        #region constructor
+        public RoomDB() : base()
+        {
+            rooms = new Collection<Room>();
+            FillDataSet(sqlLocal1, guestTable);
+            AddToCollection(guestTable);
+        }
+        #endregion
+        #region Utility Methods
+        public DataSet GetDataSet()
+        {
+            return dataSet;
+        }
+
+        private void AddToCollection(string table)
+        {
+            DataRow myRow = null;
+            Room room;
+
+            foreach (DataRow row in dataSet.Tables[table].Rows)
+            {
+                myRow = row;
+
+                if (!(myRow.RowState == DataRowState.Deleted))
+                {
+                    room = new Room();
+                    room.Id = Convert.ToString(myRow["id"]).TrimEnd();
+                   
+                    rooms.Add(room);
+
+                }
+            }
+        }
+
+        private void FillRow(DataRow row, Room room, DB.DBOperation operation)
+        {
+            if (operation == DB.DBOperation.Add)
+            {
+
+                row["id"] = room.Id;
+             
+
+            }
+            row["roomTypeId"] = room.roomTypeId;
+            row["roomStatusId"] = room.RoomStatusId;
+            row["promotionId"] = room.PromotionId;
+            row["roomNumber"] = room.RoomNumber;
+            row["rate"] = room.Rate;
+           
+        }
+        private int FindRow(Room room, string table)
+        {
+            int rowIndex = 0;
+            DataRow myRow;
+            int returnValue = -1;
+            foreach (DataRow row in dataSet.Tables[table].Rows)
+            {
+                myRow = row;
+                if (myRow.RowState != DataRowState.Deleted)
+                {
+                    if (room.Id == Convert.ToString(dataSet.Tables[table].Rows[rowIndex]["id"]))
+                    {
+                        returnValue = rowIndex;
+                    }
+                }
+                rowIndex++;
+            }
+
+            return returnValue;
+        }
+        #endregion
+        #region CRUD operataions
+        public void DataSetChange(Room room, DB.DBOperation operation)
+        {
+
+            DataRow row = null;
+            string dataTable = guestTable;
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    row = dataSet.Tables[dataTable].NewRow();
+                    FillRow(row, room, operation);
+                    dataSet.Tables[dataTable].Rows.Add(row);
+                    break;
+                case DB.DBOperation.Edit:
+                    row = dataSet.Tables[dataTable].Rows[FindRow(room, dataTable)];
+                    FillRow(row, room, operation);
+                    break;
+                case DB.DBOperation.Delete:
+                    int rowIndexToDelete = FindRow(room, dataTable);
+                    if (rowIndexToDelete != -1)
+                    {
+                        dataSet.Tables[dataTable].Rows.RemoveAt(rowIndexToDelete);
+                    }
+                    break;
+
+            }
+        }
+
+        #endregion
+
+        #region build parameters, create commands and update database
+        private void insert(Room room)
+        {
+            SqlParameter param = default(SqlParameter);
+            param = new SqlParameter("@id", SqlDbType.Int, 50, "id");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@roomTypeId", SqlDbType.NChar, 13, "roomTypeId");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("roomStatusId", SqlDbType.NChar, 13, "roomStatusId");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@promotionId", SqlDbType.NVarChar, 50, "promotionId");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@roomNumber", SqlDbType.NVarChar, 50, "roomNumber");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@rate", SqlDbType.NVarChar, 255, "rate");
+            dataAdapter.InsertCommand.Parameters.Add(param);
+
+        }
+
+
+        private void update(Guest guest)
+        {
+            SqlParameter param = default(SqlParameter);
+
+        }
+
+      
+
+        public bool UpdateDataSource(Room room)
+        {
+            bool sucess = true;
+         
+            update(room);
+
+
+            UpdateDataSource(sqlLocal1, guestTable);
+
+            return sucess;
+        }
+        #endregion
+
+
+        #region getters and setters
+        public Collection<Room> AllRooms
+        {
+            get
+            {
+                return rooms;
+            }
+        }
+        #endregion
+    }
+}
