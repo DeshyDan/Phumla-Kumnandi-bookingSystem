421110310d45425123d65270bb16ed202a580da2
Deshy Dan <deshy733@gmail.com>
2023-10-06 12:07:47 +0200

    Added the login screen

--- Phumla Kumnandi Hotel Reservation System/Login.cs
+++ Phumla Kumnandi Hotel Reservation System/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Phumla_Kumnandi_Hotel_Reservation_System
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
--- Phumla Kumnandi Hotel Reservation System/Program.cs
+++ Phumla Kumnandi Hotel Reservation System/Program.cs
@@ -16,7 +16,7 @@ namespace Phumla_Kumnandi_Hotel_Reservation_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
